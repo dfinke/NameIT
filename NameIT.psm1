6c4edcd5b197ae98ef48be48463bb3ce5a1f9200
Chris Hunt <cdhunt@gmail.com>
2016-09-22 10:38:43 -0400

    Add Get-RandomValue cmdlet

--- NameIT.psm1
+++ NameIT.psm1
@@ -1,5 +1,3 @@
-
-
 <#
 .Synopsis
    Utilize Invoke-Generate to create a random value type
@@ -98,6 +96,80 @@ function Invoke-Generate {
             }
         }) -join ''
     }
+}
+
+<#
+.Synopsis
+   Utilize Invoke-Generate to create a random value type
+
+.DESCRIPTION
+   NameIt returns strings including unecessary zeros in numbers. Get-RandomValue returns a specified values type. 
+
+.PARAMETER Template
+   A Nameit template string.
+
+.PARAMETER As
+   A type name specifying the return type of the command.
+
+.PARAMETER Alphabet
+   A set of alpha characters used to generate random strings.
+
+.PARAMETER Numbers
+   A set of digit characters used to generate random numerics.
+
+.EXAMPLE
+   PS C:\> 1..3 | % {Get-RandomValue "###.##" -as double}
+   75.41
+   439.92
+   195.55
+
+.EXAMPLE
+   PS C:\> 1..3 | % {Get-RandomValue "#.#.#" -as version}
+
+   Major  Minor  Build  Revision
+   -----  -----  -----  --------
+   1      3      1      -1      
+   2      2      5      -1      
+   7      1      0      -1      
+#>
+function Get-RandomValue
+{
+    [CmdletBinding()]
+    [Alias()]
+    Param
+    (
+        [Parameter(Mandatory,Position=0)]
+        [string]
+        $Template,
+
+        [Parameter(Position=1)]
+        [Type]
+        $As,
+
+        [Parameter(Position=2)]
+        [string]
+        $Alphabet,
+
+        [Parameter(Position=3)]
+        [string]        
+        $Numbers
+    )
+
+    $stringValue = Invoke-Generate @PSBoundParameters
+
+    if ( $PSBoundParameters.ContainsKey('As') )
+    {
+        $returnValue = $stringValue -as $As
+        if ($null -eq $returnValue) {
+            Write-Warning "Could not cast '$stringValue' to [$($As.Name)]"
+        }
+    }
+    else
+    {
+        $returnValue = $stringValue
+    }
+
+    $returnValue
 }
 
 function Get-RandomChoice {
