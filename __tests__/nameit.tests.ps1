309bb34d084f9766c60f14b538a14c97f8d88c5a
dfinke <finked@hotmail.com>
2018-08-01 14:37:51 -0400

    update param

--- __tests__/nameit.tests.ps1
+++ __tests__/nameit.tests.ps1
@@ -1,12 +1,8 @@
-# $module = Resolve-Path  "$PSScriptRoot/../NameIt.psd1"
-# Import-Module $module
-# Import-Module $PSScriptRoot/../NameIt
-
 Import-Module $module "$PSScriptRoot/../NameIT.psd1"
 
 Describe "NameIT Tests" {
     It "Should generate five entries" {
-        $actual = Invoke-Generate "[person]" -Count 5
+        $actual = Invoke-Generate -Template "[person]" -Count 5
 
         $actual.count | Should Be 5
     }
