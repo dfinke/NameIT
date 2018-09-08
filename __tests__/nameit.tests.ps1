76de1d3be296ed5d9b7268687b4925db96b5c844
dfinke <finked@hotmail.com>
2018-09-08 16:43:34 -0400

    try failing on v6 on any OS

--- __tests__/nameit.tests.ps1
+++ __tests__/nameit.tests.ps1
@@ -6,4 +6,8 @@ Describe "NameIT Tests" {
 
         $actual.count | Should Be 5
     }
+
+    It "Should pass in v5 and fail in on all OSes" {
+        (Get-WmiObject win32_bios) | Should Not Be Null
+    }
 }
