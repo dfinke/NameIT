5c6e8b7962bbe2cff8c99579a9fa86a24f577b62
dfinke <finked@hotmail.com>
2018-09-08 17:44:57 -0400

    tweak

--- __tests__/nameit.tests.ps1
+++ __tests__/nameit.tests.ps1
@@ -8,10 +8,6 @@ Describe "NameIT Tests" {
         $actual.count | Should Be 5
     }
 
-    # It "Should pass in v5 and fail in on all OSes" {
-    #     (Get-WmiObject win32_bios) | Should Not Be Null
-    # }
-
     # It "Should pass all versions all OSes" {
     #     (Get-CimInstance win32_bios) | Should Not Be Null
     # }
