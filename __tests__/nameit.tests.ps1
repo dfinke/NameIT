abfa5b81438b69a186e0ce94c0693db714cb553f
dfinke <finked@hotmail.com>
2018-09-08 16:59:20 -0400

    added test

--- __tests__/nameit.tests.ps1
+++ __tests__/nameit.tests.ps1
@@ -1,6 +1,11 @@
 Import-Module "$PSScriptRoot/../NameIT.psd1"
 
 Describe "NameIT Tests" {
+
+    AfterAll {
+        Remove-Item -Recurse -Force "$env:TEMP\TestThePath" -ErrorAction SilentlyContinue
+    }
+
     It "Should generate five entries" {
         $actual = Invoke-Generate -Template "[person]" -Count 5
 
@@ -10,4 +15,11 @@ Describe "NameIT Tests" {
     # It "Should pass in v5 and fail in on all OSes" {
     #     (Get-WmiObject win32_bios) | Should Not Be Null
     # }
+
+    It "Should fail on the path for Linux" {
+        $targetPath = "$env:TEMP\TestThePath"
+        $null = mkdir $targetPath
+
+        (Test-Path $targetPath) | Should Be $true
+    }
 }
