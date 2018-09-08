2f71238ae0237effa5931169c99ef12df31af298
dfinke <finked@hotmail.com>
2018-09-08 17:29:25 -0400

    added file creation

--- __tests__/nameit.tests.ps1
+++ __tests__/nameit.tests.ps1
@@ -15,4 +15,11 @@ Describe "NameIT Tests" {
     It "Should pass all versions all OSes" {
         (Get-CimInstance win32_bios) | Should Not Be Null
     }
+
+    It "Should pass all versions all OSes" {
+        "" | Set-Content "$PSScriptRoot\test.txt"
+
+        (Test-Path "$PSScriptRoot\test.txt") | Should Be $true
+    }
+
 }
