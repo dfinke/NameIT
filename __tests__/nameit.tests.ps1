bc502f9eb7b9c4ead6e94465f01f9ae3a147a1df
Doug Finke <finked@hotmail.com>
2018-09-16 11:46:33 -0400

    test file exists

--- __tests__/nameit.tests.ps1
+++ __tests__/nameit.tests.ps1
@@ -7,18 +7,18 @@ Describe "NameIT Tests" {
         $actual.count | Should Be 5
     }
 
-    # It "Should pass all versions all OSes" {
+    # It "Should pass on 5.1 and 6" {
     #     (Get-WmiObject win32_bios) | Should Not Be Null
     # }
 
-    It "Should pass all versions all OSes" {
-        (Get-CimInstance win32_bios) | Should Not Be Null
-    }
-
-    # It "Should fail on Linux" {
-    #     "" | Set-Content "$PSScriptRoot\Test.txt"
-    #     (Test-Path "$PSScriptRoot\test.txt") | Should Be $true
+    # It "Should pass 5.1 and 6" {
+    #     (Get-CimInstance win32_bios) | Should Not Be Null
     # }
+
+    It "Should fail on Linux" {
+        "" | Set-Content "$PSScriptRoot\Test.txt"
+        (Test-Path "$PSScriptRoot\test.txt") | Should Be $true
+    }
 
     # It "Should pass on Linux - correct case" {
     #     "" | Set-Content "$PSScriptRoot\test.txt"
