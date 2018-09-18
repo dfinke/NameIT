5a7c0223ecd1d8f8bbe3e836a2d281e83446617a
Doug Finke <finked@hotmail.com>
2018-09-17 20:36:10 -0400

    fxied with cim

--- __tests__/nameit.tests.ps1
+++ __tests__/nameit.tests.ps1
@@ -7,13 +7,13 @@ Describe "NameIT Tests" {
         $actual.count | Should Be 5
     }
 
-    It "Should pass on 5.1 and 6" {
-        (Get-WmiObject win32_bios) | Should Not Be Null
-    }
-
-    # It "Should pass 5.1 and 6" {
-    #     (Get-CimInstance win32_bios) | Should Not Be Null
+    # It "Should pass on 5.1 and 6" {
+    #     (Get-WmiObject win32_bios) | Should Not Be Null
     # }
+
+    It "Should pass 5.1 and 6" {
+        (Get-CimInstance win32_bios) | Should Not Be Null
+    }
 
     # It "Should fail on Linux" {
     #     "" | Set-Content "$PSScriptRoot\Test.txt"
