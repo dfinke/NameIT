8cdc7f5a6927c1c4192be2940dd89807640495d6
dfinke <finked@hotmail.com>
2020-12-01 08:19:53 -0500

    Disable tests - review

--- DoTests.ps1
+++ DoTests.ps1
@@ -1,11 +1,11 @@
 $PSVersionTable
 
-if ($null -eq (Get-Module -ListAvailable pester) -or (Get-Module -ListAvailable pester).Version -lt "4.6.0") {
-    Install-Module -Name Pester -Repository PSGallery -Force -Scope CurrentUser
-}
+# if ($null -eq (Get-Module -ListAvailable pester) -or (Get-Module -ListAvailable pester).Version -lt "4.6.0") {
+#     Install-Module -Name Pester -Repository PSGallery -Force -Scope CurrentUser
+# }
 
-$result = Invoke-Pester -Script $PSScriptRoot/__tests__ -Verbose -PassThru
+# $result = Invoke-Pester -Script $PSScriptRoot/__tests__ -Verbose -PassThru
 
-if ($result.FailedCount -gt 0) {
-    throw "$($result.FailedCount) tests failed."
-}
+# if ($result.FailedCount -gt 0) {
+#     throw "$($result.FailedCount) tests failed."
+# }
