5f43f15d94a5e1eede47fb291288e1fce2ddbc8a
dfinke <finked@hotmail.com>
2021-01-03 18:23:13 -0500

    Add `month` custom data

--- InstallModule.ps1
+++ InstallModule.ps1
@@ -1,3 +1,16 @@
-$fullPath = 'C:\Program Files\WindowsPowerShell\Modules\NameIT'
+param ($fullPath)
 
-Robocopy . $fullPath /mir /XD .vscode .git examples testimonials images spikes /XF appveyor.yml .gitattributes .gitignore
+# $fullPath = 'C:\Program Files\WindowsPowerShell\Modules\NameIT'
+
+if (-not $fullPath) {
+    $fullpath = $env:PSModulePath -split ":(?!\\)|;|," |
+        Where-Object {$_ -notlike ([System.Environment]::GetFolderPath("UserProfile")+"*") -and $_ -notlike "$pshome*"} |
+            Select-Object -First 1
+            $fullPath = Join-Path $fullPath -ChildPath "NameIT"
+}
+
+Push-location $PSScriptRoot
+Robocopy . $fullPath /mir /XD .vscode .git CI __tests__ data mdHelp /XF appveyor.yml azure-pipelines.yml .gitattributes .gitignore filelist.txt install.ps1 InstallModule.ps1
+Pop-Location
+
+#Robocopy . $fullPath /mir /XD .vscode .git examples testimonials images spikes /XF appveyor.yml .gitattributes .gitignore
--- customData/customData.ps1
+++ customData/customData.ps1
@@ -1,19 +1,21 @@
 @{
-    Region      = echo North East South West
+    Region      = Write-Output North East South West
 
-    Food        = echo Pizza Pancakes Chinese
+    Food        = Write-Output Pizza Pancakes Chinese
 
-    Fruit       = echo lemon lime peach apple banana
+    Fruit       = Write-Output lemon lime peach apple banana
 
-    Response    = echo Yes No Maybe "Not Sure"
+    Response    = Write-Output Yes No Maybe "Not Sure"
 
-    Ingredients = echo Flour Sugar Salt Milk Eggs Butter
+    Ingredients = Write-Output Flour Sugar Salt Milk Eggs Butter
 
-    USCapital   = echo Montgomery Juneau Phoenix Little Rock Sacramento Denver Hartford Dover Washington Tallahassee Atlanta Honolulu Boise Springfield Indianapolis Des Moines Topeka Frankfort Baton Rouge Augusta Annapolis Boston Lansing Saint Paul Jackson Jefferson City Helena Lincoln Carson City Concord Trenton Santa Fe Albany Raleigh Bismarck Columbus Oklahoma City Salem Harrisburg Providence Columbia Pierre Nashville Austin Salt Lake City Montpelier Richmond Olympia Charleston Madison Cheyenne
+    Month       = 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
 
-    President   = echo "George Washington", "John Adams", "Thomas Jefferson", "James Madison", "James Monroe", "John Quincy AdamsAndrew Jackson", "Martin Van Buren", "William Henry Harrison", "John Tyler", "James K. Polk", "Zachary Taylor", "Millard Fillmore", "Franklin Pierce", "James Buchanan", "Abraham Lincoln", "Andrew Johnson", "Ulysses S. Grant", "Rutherford B. Hayes", "James A. Garfield", "Chester Arthur", "Grover Cleveland", "Benjamin Harrison", "Grover Cleveland", "William McKinley", "Theodore Roosevelt", "William Howard Taft", "Woodrow Wilson", "Warren G. Harding", "Calvin Coolidge", "Herbert Hoover", "Franklin D. Roosevelt", "Harry S. Truman", "Dwight D. Eisenhower", "John F. Kennedy", "Lyndon B. Johnson", "Richard Nixon", "Gerald Ford", "Jimmy Carter", "Ronald Reagan", "George Bush", "Bill Clinton", "George W. Bush", "Barack Obama"
+    USCapital   = Write-Output Montgomery Juneau Phoenix Little Rock Sacramento Denver Hartford Dover Washington Tallahassee Atlanta Honolulu Boise Springfield Indianapolis Des Moines Topeka Frankfort Baton Rouge Augusta Annapolis Boston Lansing Saint Paul Jackson Jefferson City Helena Lincoln Carson City Concord Trenton Santa Fe Albany Raleigh Bismarck Columbus Oklahoma City Salem Harrisburg Providence Columbia Pierre Nashville Austin Salt Lake City Montpelier Richmond Olympia Charleston Madison Cheyenne
 
-     # Louise Hay affirmations
+    President   = Write-Output "George Washington", "John Adams", "Thomas Jefferson", "James Madison", "James Monroe", "John Quincy AdamsAndrew Jackson", "Martin Van Buren", "William Henry Harrison", "John Tyler", "James K. Polk", "Zachary Taylor", "Millard Fillmore", "Franklin Pierce", "James Buchanan", "Abraham Lincoln", "Andrew Johnson", "Ulysses S. Grant", "Rutherford B. Hayes", "James A. Garfield", "Chester Arthur", "Grover Cleveland", "Benjamin Harrison", "Grover Cleveland", "William McKinley", "Theodore Roosevelt", "William Howard Taft", "Woodrow Wilson", "Warren G. Harding", "Calvin Coolidge", "Herbert Hoover", "Franklin D. Roosevelt", "Harry S. Truman", "Dwight D. Eisenhower", "John F. Kennedy", "Lyndon B. Johnson", "Richard Nixon", "Gerald Ford", "Jimmy Carter", "Ronald Reagan", "George Bush", "Bill Clinton", "George W. Bush", "Barack Obama"
+
+    # Louise Hay affirmations
     affirmation = $(
         "Life loves me!"
         "All is well in my world. Everything is working out for my highest good. Out of this situation only good will come. I am safe!"
