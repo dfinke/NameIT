9a94a3e56a1424af94cbeb453e7726a6eb4df879
Grant Steinfeld <grant.steinfeld.tech@gmail.com>
2021-09-06 07:12:11 -0400

    small corrections

--- Examples/ShowAfrikaansNames.ps1
+++ Examples/ShowAfrikaansNames.ps1
@@ -2,5 +2,5 @@ Import-Module "$PSScriptRoot\..\nameit.psd1" -Force
 
 Invoke-Generate -Template "[person]" -Count 5  -Culture af
 Invoke-Generate -Template "[person female first]" -Count 5  -Culture af
-Invoke-Generate -Template "[person male first]" -Count 5  -Culture af
+Invoke-Generate -Template "[person male last]" -Count 5  -Culture af
 Invoke-Generate -Template "[color]" -Count 5  -Culture af
