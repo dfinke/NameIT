8097afb7d84ba78a22d5003e07077e9386c8be23
Göran Hanell <goran@imagineto.be>
2021-09-20 22:44:29 +0200

    Add color and email tests + update email gen

--- Generators/urls.ps1
+++ Generators/urls.ps1
@@ -1,7 +1,13 @@
 function email {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [cultureinfo]
+        $Culture = [cultureinfo]::CurrentCulture
+    )
     # $tlds = 'com', 'com', 'com', 'net', 'org', 'es', 'es', 'es'
 
     $domain = 'gmail.com', 'yahoo.com', 'hotmail.com'
 
-    "{0}@{1}" -f (person).tolower().replace(' ', '.'), ($domain | Get-Random)
+    "{0}@{1}" -f (person -Culture $Culture).tolower().replace(' ', '.'), ($domain | Get-Random)
 }
--- __tests__/email.tests.ps1
+++ __tests__/email.tests.ps1
@@ -1,18 +1,35 @@
 Import-Module  "$PSScriptRoot\..\NameIT.psd1" -Force
 
-Describe "email Generation Test" {
+Describe "email Generation Test (en culture)" {
 
     BeforeEach {
         $null = Get-Random -SetSeed 1        
     }
 
     It "Tests returning a email title" {
-        $actual = Invoke-Generate '[email]' -Count 5
+        $actual = Invoke-Generate '[email]' -Count 5 -Culture en
 
         $actual[0] | Should -BeExactly 'holly.faulkner@hotmail.com'
         $actual[1] | Should -BeExactly 'raina.keith@hotmail.com'
         $actual[2] | Should -BeExactly 'henry.hernandez@yahoo.com'
         $actual[3] | Should -BeExactly 'rory.figueroa@yahoo.com'
         $actual[4] | Should -BeExactly 'leon.tapia@gmail.com'
+    }
+}
+Describe "email Generation Test (sv culture)" -Tag CultureSv {
+
+    BeforeEach {
+        $null = Get-Random -SetSeed 1        
+    }
+
+    It "Tests returning a email title" {
+
+        $actual = Invoke-Generate '[email]' -Count 5 -Culture sv
+
+        $actual[0] | Should -BeExactly 'hanna.falk@hotmail.com'
+        $actual[1] | Should -BeExactly 'rebecca.johansson@hotmail.com'
+        $actual[2] | Should -BeExactly 'jan.holm@yahoo.com'
+        $actual[3] | Should -BeExactly 'rut.falk@yahoo.com'
+        $actual[4] | Should -BeExactly 'marcus.ström@gmail.com'
     }
 }
