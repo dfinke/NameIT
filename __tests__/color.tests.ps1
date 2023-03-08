8114b86fca429ff8f19ec158c4608dd723065b69
Mikey Bronowski <MikeyBronowski@gmail.com>
2023-03-08 23:15:08 +0000

    Update color.tests.ps1

--- __tests__/color.tests.ps1
+++ __tests__/color.tests.ps1
@@ -20,6 +20,26 @@ Describe "color Generation Test (en culture)" -Tag color, CultureEn {
         $actual[4] | Should -BeExactly 'violet'
     }
 }
+
+Describe "color Generation Test (pl culture)" -Tag color, CulturePl {
+
+    BeforeEach {
+        $null = Get-Random -SetSeed 1        
+    }
+
+    It "Generates a color (pl culture)" {
+        $actual = Invoke-Generate '[color]' -Count 5 -Culture pl
+
+        $actual.Count | Should -Be 5
+                
+        $actual[0] | Should -BeExactly 'beżowy'
+        $actual[1] | Should -BeExactly 'biały'
+        $actual[2] | Should -BeExactly 'brązowy'
+        $actual[3] | Should -BeExactly 'brunatny'
+        $actual[4] | Should -BeExactly 'bursztynowy'
+    }
+}
+
 Describe "color Generation Test (sv culture)" -Tag color, CultureSv {
 
     BeforeEach {
